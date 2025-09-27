8ef00e16b8b7d956ff86e4bb783ab97c517edbd7
miy3ll <miyellfernandez0703@gmail.com>
2025-09-27 10:19:09 -0400

    variables, operadores matematicos y de comparacion

---
+++ operadores_matematicos.py
@@ -0,0 +1,11 @@
+from variables import z, x, y
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+print(z)
+
+print(f"variable z: {z}")
+print(f"variable x + y = {x + y}")
---
+++ variables.py
@@ -0,0 +1,8 @@
+x = 2
+y = 3
+z = x + y
+nombre = "Miyell"
+cedula = "30150512"
+
+print(nombre)
+
