ba3260713d7468116b7ad52e9424fe4f4822ea86
miy3ll <miyellfernandez0703@gmail.com>
2025-10-04 11:20:58 -0400

    ciclos_for

---
+++ ciclo_for.py
@@ -0,0 +1,22 @@
+# lista de fruras
+fruits = ["manzana", "banana", "cereza"]
+
+# recorrer la lista con un ciclo for
+# for fruit in fruits:
+#     # print(fruit)
+#     if fruit == "manzana":
+#         print("encontré una manzana")
+#         print()
+#     break
+    #else:
+        #print("no hay una manzana")
+
+#for fruit in len(fruits):
+#print(len(fruits))
+
+# for elemento in range(3):
+#     print(elemento)
+
+for elemento in range(len(fruits)):
+    print(fruits[elemento])
+    print(elemento)
