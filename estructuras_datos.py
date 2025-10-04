be06a8d57ef2461f306d56f12693b1cce7a2c9bf
miy3ll <miyellfernandez0703@gmail.com>
2025-10-04 10:26:18 -0400

    feacture/estructuras_fatos

---
+++ estructuras_datos.py
@@ -0,0 +1,57 @@
+# estructuras de datos en python
+# enteros
+number = 10
+
+# flotantes
+pi = 3.14
+# print(type(number)) # <class 'int'>
+
+# cadenas de texto
+greeting = "Hola, mundo"
+# print(type(greeting)) # <class 'str'>
+
+# booleanos
+is_active = True
+# print(type(is_active)) # <class 'bool'>
+
+# listas
+fruits = [
+    "manzana", 
+    "banana", 
+    "cereza"
+    ]
+# print(fruits)
+# agregar un elemento a la lista
+fruits.append("naranja")
+
+# print(fruits)
+
+# print(fruits[1:])
+# print(fruits[-1])
+
+fruits[1] = "kiwi"
+# print(fruits)
+
+# tuplas
+nombres = (
+            "Alice", 
+            "Bob", 
+            "Charlie",
+            "Diana",
+            )
+# print(nombres[-1])
+
+# nombres[0] = "Eve"  # Esto generará un error porque las tuplas son inmutables
+
+# diccionarios
+persona = {
+    "nombre": "Alice",
+    "edad": 30,
+    "ciudad": "Wonderland",
+    "comida_favorita": ["tarta", "té"]
+}
+print(persona["nombre"])
+print(persona["edad"])
+
+persona["profesion"] = "adventurer"
+print(persona)
